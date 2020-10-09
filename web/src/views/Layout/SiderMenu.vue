8ae53af1680ad70d60e3fa8a63189abdbcdf4acc
hwb2017 <1003673802@qq.com>
2020-10-10 07:03:03 +0800

    调整导航栏展示

--- web/src/views/Layout/SiderMenu.vue
+++ web/src/views/Layout/SiderMenu.vue
@@ -1,67 +1,25 @@
 <template>
   <a-menu 
     theme="dark" 
-    :defaultSelectedKeys="['1']" 
-    :defaultOpenKeys="['sub1']"
+    :defaultSelectedKeys="['virtualmachine']" 
+    :defaultOpenKeys="['infrastructure']"
     mode="inline"
   >
     <template v-for="item in menuData">
       <a-menu-item 
-        v-if="!item.children"
+        v-if="item.standalone"
         :key="item.name"
         @click="() => $router.push({ path: item.path, query: $route.query })"
       >
         {{ item.meta.title }}
       </a-menu-item>
       <sub-menu
-        v-else
+        v-if="item.children"
         :key="item.name"
         :menu-info="item"
       >
       </sub-menu>
     </template>
-    <a-sub-menu key="sub1">
-      <span slot="title"><a-icon type="database" /><span>基础设施管理</span></span>
-      <a-menu-item key="1" @click="routeToVirtualMachines">
-        虚拟机
-      </a-menu-item>
-      <a-menu-item key="10" @click="routeToVMLifecycle">
-        虚拟机生命周期
-      </a-menu-item>      
-      <a-menu-item key="2">
-        对象存储
-      </a-menu-item>
-    </a-sub-menu>
-    <a-sub-menu key="sub2">
-      <span slot="title"><a-icon type="appstore" /><span>应用管理</span></span>
-      <a-menu-item key="3">
-        概览
-      </a-menu-item>
-      <a-menu-item key="4">
-        服务树
-      </a-menu-item>
-    </a-sub-menu>
-    <a-sub-menu key="sub3">
-      <span slot="title"><a-icon type="api" /><span>OpenAPI列表</span></span>
-      <a-menu-item key="5">
-        监控告警域
-      </a-menu-item>
-      <a-menu-item key="6">
-        资源管理域
-      </a-menu-item>
-    </a-sub-menu>
-    <a-sub-menu key="sub4">
-      <span slot="title"><a-icon type="setting" /><span>系统设置</span></span>
-      <a-menu-item key="7">
-        权限管理
-      </a-menu-item>
-      <a-menu-item key="8">
-        云API管理
-      </a-menu-item>
-      <a-menu-item key="9">
-        LDAP设置
-      </a-menu-item>      
-    </a-sub-menu>    
   </a-menu>  
 </template>
 
@@ -78,33 +36,27 @@ export default {
     };
   },
   methods: {
-    getMenuData(routes = []) {
+    getMenuData(routes = [], depth = 0) {
       const menuData = [];
       for (let item of routes) {
         if (item.name && !item.hideInMenu) {
-            const menuitem = { ...item };
-            delete menuitem.children;
+            item.depth = depth;
+            if (item.depth==1 && !item.children) {
+              item.standalone=true;
+            }
             menuData.push(item);
             if (item.children) {
-              menuData.push(...this.getMenuData(item.children));
+              menuData.push(...this.getMenuData(item.children, depth+1));
             }
         } else if (
           item.children &&
           !item.hideInMenu
         ) {
-          menuData.push(...this.getMenuData(item.children));
+          menuData.push(...this.getMenuData(item.children, depth+1));
         }
       }
       return menuData;
     },
-    routeToVirtualMachines() {
-        this.$router.push({path:"/infrastructure/virtualmachines"})
-        console.log(this.menuData);
-    },
-    routeToVMLifecycle() {
-        this.$router.push({path:"/infrastructure/vmlifecycles"})
-        console.log(this.menuData);
-    }
   }
 }
 </script>
